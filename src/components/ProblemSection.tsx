8a7950f4c0a9ddbb6d0d3222e748d6a81c7708c9
Zoflux <henrijung@Henris-MacBook-Air.local>
2025-12-25 21:59:37 +0100

    Bigger boilr logo in slider (h-32 = 128px)

--- src/components/ProblemSection.tsx
+++ src/components/ProblemSection.tsx
@@ -144,7 +144,7 @@ export function ProblemSection() {
                                     <img
                                         src="/boilr-logo-black.png"
                                         alt="boilr"
-                                        className="h-20 object-contain relative z-10 brightness-0 invert"
+                                        className="h-32 object-contain relative z-10 brightness-0 invert"
                                     />
                                 </div>
                             </div>
